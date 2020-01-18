0a418d6d53b530d9c40b66bffd26f3cc8d99bede
Andrey Mashukov <andre.mashukov@gmail.com>
2020-01-18 19:01:54 +0800

    create new app

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import React from 'react';
-import './App.scss';
-import jQuery from 'jquery/dist/jquery.js';
-import 'bootstrap/dist/js/bootstrap.js';
-import { BrowserRouter } from "react-router-dom";
-import Store from './store/Store'
-import { Layout } from './shared/Layout'
-import { Footer } from './views/Footer'
+import logo from './logo.svg';
+import './App.css';
 
-window.jQuery = jQuery;
-class App extends React.Component {
-  componentDidMount() {
-    document.title = 'Ecommerce Template';
-    localStorage.setItem('products', JSON.stringify({
-      products: [
-        {
-          item_id: 1,
-          key: 'walkie-talkie-wireless-earpiece',
-          collection_name: 'Earphone & Headphone',
-          name: 'Walkie Talkie',
-          details: 'Sports Earphone, Swimming headset, Bone conduction headset',
-          title: 'Walkie Talkie Wireless Earpiece',
-          price: {
-            price: 60
-          }            
-        },
-        {
-          item_id: 2,
-          key: 'mini-stealth-stereo',
-          collection_name: 'Earphone & Headphone',
-          name: 'Mini Stealth Stereo',
-          details: 'True Wireless Bluetooth Headphones Earphone RV11 with mic',
-          title: 'Mini Stealth Stereo',
-          price: {
-            price: 10
-          }           
-        }
-      ]
-  }));
-
-  }
-  render() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-              <Store>
-                <Layout />
-                <Footer />
-              </Store>
-            </BrowserRouter>
-        </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/views/PageNotFound.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default () => (
-    <h3>Opp! Page error 404 not found.</h3>
-)
