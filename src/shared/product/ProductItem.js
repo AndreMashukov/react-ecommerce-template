769acd30f8b0f72db2cf149388a7df541dcce04e
Andrey Mashukov <andre.mashukov@gmail.com>
2020-01-25 15:54:41 +0800

    minor changes

--- src/shared/product/ProductItem.js
+++ src/shared/product/ProductItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import CartContext from '../../store/CartContext';
 import { ProductActualPrices } from '../../shared/Utils';
 import { Grid, Typography } from '@material-ui/core';
 
@@ -10,30 +9,24 @@ export const ProductItem = props => {
   const pricing = ProductActualPrices(product);
 
   return (
-    <CartContext.Consumer>
-      {cart => {
-        return (
-          <div>
-              <Grid
-                container
-                direction="column"
-                justify="space-around"
-                alignItems="flex-start"
-                spacing={3}
-              >
-                <Grid item>
-                  <Link to={'/product/' + product.item_id}>
-                    <Typography variant="h6" color="primary">
-                      {product.name}
-                    </Typography>
-                  </Link>
-                </Grid>
-                <Grid item>${pricing.price}</Grid>
-                <Grid item>{product.collection_name}</Grid>
-              </Grid>
-          </div>
-        );
-      }}
-    </CartContext.Consumer>
+    <div>
+      <Grid
+        container
+        direction="column"
+        justify="space-around"
+        alignItems="flex-start"
+        spacing={3}
+      >
+        <Grid item>
+          <Link to={'/product/' + product.item_id}>
+            <Typography variant="h6" color="primary">
+              {product.name}
+            </Typography>
+          </Link>
+        </Grid>
+        <Grid item>${pricing.price}</Grid>
+        <Grid item>{product.collection_name}</Grid>
+      </Grid>
+    </div>
   );
 };
